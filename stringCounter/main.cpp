30d135def75811f4b18f82828fa1bb2b5166bba7
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-11-20 13:29:44 -0800

    Initial Commit

---
+++ stringCounter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stringCounter
+//
+//  Created by Abel Christy Anand on 11/20/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
